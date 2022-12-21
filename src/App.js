8457c405c50ef5be1a5b24ad35f032678bbcbd7e
matsuik <matsuik@scala-partners.com>
2022-12-21 17:19:09 +0900

    認証機能UIを追加、コメントアウトも追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// スタイルシートをインポート
+import "@aws-amplify/ui-react/styles.css";
+//  amplify本体のオブジェクト
+import { Amplify } from "aws-amplify";
+// UI関係のモジュールからwithAuthenticator関数をインポート
+import { withAuthenticator } from "@aws-amplify/ui-react";
+// 自動生成される情報をインポート
+import aws_exports from "./aws-exports";
+
+// 設定情報を反映（バックエンドとうまくやり取りするためのもの）
+Amplify.configure(aws_exports);
 
 function App() {
   return (
@@ -9,12 +20,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
@@ -22,4 +28,5 @@ function App() {
   );
 }
 
-export default App;
+// サインインしていたらAppをexport, 未サインインならサインインに必要なフォームをexport
+export default withAuthenticator(App);
