eee7fdec452330fe546f52414dbab24cf04c078b
matsuik <matsuik@scala-partners.com>
2022-12-28 17:07:28 +0900

    chater5.1とサンプル追加

--- src/App.js
+++ src/App.js
@@ -3,95 +3,107 @@ import "./App.css";
 // スタイルシートをインポート
 import "@aws-amplify/ui-react/styles.css";
 //  Amplify本体のオブジェクト、サインアウト用のAuthオブジェクト
-import { Amplify, Auth } from "aws-amplify";
+import { Amplify, Auth, DataStore } from "aws-amplify";
 // UI関係のモジュールからwithAuthenticator関数をインポート
 import { withAuthenticator } from "@aws-amplify/ui-react";
 // 自動生成される情報をインポート
 import aws_exports from "./aws-exports";
-import { Header } from "./ui-components";
+import { Header, BoardComponent, BoardComponentCollection } from "./ui-components";
 import { useState, useEffect } from "react";
+import { Board } from "./models";
 
 // 設定情報を反映（バックエンドとうまくやり取りするためのもの）
 Amplify.configure(aws_exports);
 
-function App() {
-  const [msg, setMsg] = useState("ok");
-  const onClick = (value) => {
-    setMsg("You typed:" + value);
-  };
+// const content1 = <BoardComponentCollection />;
+const content2 = <p>タブ2のコンテンツ</p>;
+const content3 = <p>タブ3のコンテンツ</p>;
+const content4 = <p>タブ4のコンテンツ</p>;
 
-  const [flag, setFlag] = useState(false);
-  const onChange_flg = (event) => {
-    setFlag(event.target.checked);
+function App() {
+  const [content1, setContent1] = useState("Test");
+  const [input, setInput] = useState("");
+  const [find, setFind] = useState(input);
+  const doChange = (event) => {
+    setInput(event.target.value);
   };
+  const doFilter = (event) => {
+    setFind(input);
+  };
+  useEffect(() => {
+    DataStore.query(Board, (ob) => ob.name("contains", find)).then((values) => {
+      const data = [];
+      for (let item of values) {
+        data.push(<BoardComponent board={item} key={item.id} />);
+      }
+      setContent1(
+        <div>
+          <div className="mx-0 my-3 row">
+            <input type="text" className="form-control col" onChange={doChange} />
+            <button className="btn btn-primary col-2" onClick={doFilter}>
+              Click
+            </button>
+          </div>
+          {data}
+        </div>
+      );
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [input, find]);
 
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>＊これは、UIコンポーネントを利用した表示です。</p>
-      <Now />
+      <Now bg="red" />
       <hr />
-      <Message title="結果の表示" value={msg} />
-      <Form value="ok" onClick={onClick} />
-      <hr />
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" id="check1" onChange={onChange_flg} />
-        <label className="form-check-label" htmlFor="check1">
-          表示の切り替えボックス
-        </label>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a href="#tab1" className="nav-link active" data-bs-toggle="tab">
+            List
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#tab2" className="nav-link" data-bs-toggle="tab">
+            Create
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#tab3" className="nav-link" data-bs-toggle="tab">
+            Update
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#tab4" className="nav-link" data-bs-toggle="tab">
+            Delete
+          </a>
+        </li>
+      </ul>
+      <div className="tab-content">
+        <div id="tab1" className="my-2 tab-pane active">
+          {content1}
+        </div>
+        <div id="tab2" className="my-2 tab-pane">
+          {content2}
+        </div>
+        <div id="tab3" className="my-2 tab-pane">
+          {content3}
+        </div>
+        <div id="tab4" className="my-2 tab-pane">
+          {content4}
+        </div>
       </div>
-      {flag ? <AlertMessage title="チェックはON!" msg="チェックONのメッセージです！" /> : <BoxMessage title="チェックはOFF!" msg="チェックOFFのメッセージです！" />}
+      <p className="my-2">
+        <a className="btn btn-primary" href="." onClick={Auth.signOut}>
+          Sigh Out!
+        </a>
+      </p>
     </div>
   );
 }
 
 function Now() {
   return <p className="bg-secondary text-dark bg-opaciry-25 p-3 my-3">現在は、{new Date().getHours()}時です。</p>;
-}
-
-function AlertMessage(props) {
-  return (
-    <div className="alert alert-primary">
-      <h3>{props.title}</h3>
-      {props.msg}
-    </div>
-  );
-}
-
-function BoxMessage(props) {
-  return (
-    <div className="card">
-      <div className="card-header">{props.title}</div>
-      <div className="card-body">{props.msg}</div>
-    </div>
-  );
-}
-
-function Message(props) {
-  return (
-    <div className="alert alert-primary">
-      <h6>{props.title}</h6>
-      <p>{props.value}</p>
-    </div>
-  );
-}
-
-function Form(props) {
-  const [value, setValue] = useState(props.value);
-  const onChange = (event) => {
-    setValue(event.target.value);
-  };
-  const onClick = (evnt) => {
-    props.onClick(value);
-  };
-  return (
-    <div className="alert alert-info">
-      <input type="text" className="form-control" onChange={onChange} value={value} />
-      <button className="btn btn-primary" onClick={onClick}>
-        Click
-      </button>
-    </div>
-  );
 }
 
 // サインインしていたらAppをexport, 未サインインならサインインに必要なフォームをexport
